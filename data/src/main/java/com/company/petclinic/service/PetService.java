aed553eb85c6038931764867f044fc1c947b065c
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-08-26 14:50:08 +0430

    add release plugin #3

---
+++ data/src/main/java/com/company/petclinic/service/PetService.java
@@ -0,0 +1,11 @@
+package com.company.petclinic.service;
+
+import com.company.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+    Pet save(Pet pet);
+    Set<Pet> findAll();
+}
---
+++ data/src/main/java/com/company/petclinic/service/VetService.java
@@ -0,0 +1,11 @@
+package com.company.petclinic.service;
+
+import com.company.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+    Vet save(Vet vet);
+    Set<Vet> findAll();
+}
