12872280b69eaa85778716bf748f29db6b0e5096
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-08-29 12:07:35 +0430

    Add vets Page and Controller. Closes #7

---
+++ web/src/main/java/com/company/petclinic/controllers/VetController.java
@@ -0,0 +1,13 @@
+package com.company.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vets")
+public class VetController {
+    @RequestMapping({"/","","index","index.html"})
+    public String vets(){
+        return "vets/index";
+    }
+}
