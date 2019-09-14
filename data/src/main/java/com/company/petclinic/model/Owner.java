1cadcae5c60cbcf6b3359f7c45ec07e741428deb
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-09-14 14:41:57 +0430

    add properties. closes #28

--- data/src/main/java/com/company/petclinic/model/Owner.java
+++ data/src/main/java/com/company/petclinic/model/Owner.java
@@ -3,7 +3,35 @@ package com.company.petclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
+
+    private String addrees;
+    private String city;
+    private String tekephone;
     private Set<Pet> pets;
+
+    public String getAddrees() {
+        return addrees;
+    }
+
+    public void setAddrees(String addrees) {
+        this.addrees = addrees;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getTekephone() {
+        return tekephone;
+    }
+
+    public void setTekephone(String tekephone) {
+        this.tekephone = tekephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
