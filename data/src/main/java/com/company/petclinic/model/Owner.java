be2266312a5efef9c04199bc16c4de9be718476a
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-09-14 14:55:25 +0430

    added PetTypeMapService. closes #29

--- data/src/main/java/com/company/petclinic/model/Owner.java
+++ data/src/main/java/com/company/petclinic/model/Owner.java
@@ -4,17 +4,17 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-    private String addrees;
+    private String address;
     private String city;
-    private String tekephone;
+    private String telephone;
     private Set<Pet> pets;
 
-    public String getAddrees() {
-        return addrees;
+    public String getAddress() {
+        return address;
     }
 
-    public void setAddrees(String addrees) {
-        this.addrees = addrees;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public String getCity() {
@@ -25,12 +25,12 @@ public class Owner extends Person {
         this.city = city;
     }
 
-    public String getTekephone() {
-        return tekephone;
+    public String getTelephone() {
+        return telephone;
     }
 
-    public void setTekephone(String tekephone) {
-        this.tekephone = tekephone;
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
     }
 
     public Set<Pet> getPets() {
---
+++ data/src/main/java/com/company/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.company.petclinic.services;
+
+import com.company.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
