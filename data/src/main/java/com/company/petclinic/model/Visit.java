663bd1b24982959453ad1bc39493bc0f054f1b97
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-09-17 16:34:47 +0430

    Updated visit to use jpa . closes #38

--- data/src/main/java/com/company/petclinic/model/Visit.java
+++ data/src/main/java/com/company/petclinic/model/Visit.java
@@ -1,9 +1,6 @@
 package com.company.petclinic.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
@@ -13,7 +10,8 @@ public class Visit extends BaseEntity {
     private LocalDate date;
     @Column(name = "description")
     private String description;
-    @OneToMany
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
