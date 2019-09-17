b648734f8dcdcf5f2bf9719734d50cefe5775968
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-09-17 16:28:40 +0430

    Updated vet to use jpa . closes #37

--- data/src/main/java/com/company/petclinic/model/Vet.java
+++ data/src/main/java/com/company/petclinic/model/Vet.java
@@ -1,9 +1,14 @@
 package com.company.petclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
+    @ManyToMany
+    @JoinTable(name = "vet_specialties" ,joinColumns = @JoinColumn(name = "vet_id"),inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
