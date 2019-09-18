7468de04ccb14c284afad24e7bfec0e631bcb238
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2019-09-18 17:00:57 +0430

    Added Project Lombok to project, closes #51

--- data/src/main/java/com/company/petclinic/model/Owner.java
+++ data/src/main/java/com/company/petclinic/model/Owner.java
@@ -1,11 +1,26 @@
 package com.company.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 
     @Column(name = "address")
     private String address;
@@ -16,35 +31,4 @@ public class Owner extends Person {
     @OneToMany(mappedBy = "owner",cascade = CascadeType.ALL)
     private Set<Pet> pets = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
